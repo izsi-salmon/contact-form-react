cc13079c648711d72db2b1cdf00edce772b25192
izsi-salmon <izsi.salmon@gmail.com>
2022-04-04 12:16:16 +1200

    project set up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return(
+    <div>
+      <p>Hello world!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
